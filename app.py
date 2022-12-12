71c1e9a35e26af3b2171c0308f5913668162dc8f
Zeynep Meriç Aşık <84936243+meric2@users.noreply.github.com>
2022-12-12 04:52:41 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Patient Survival Prediction
+
+This app predicts **patient survival.**
+
+""")
+
+
+#tickerSymbol = 'GOOGL'
+#get data on this ticker
+#tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+#tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+#st.line_chart(tickerDf.Close)
+#st.line_chart(tickerDf.Volume)
+
+def user_input_features():
+    features = []#pd.dp
+    return features
